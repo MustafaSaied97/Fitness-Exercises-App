8aa672f6299e1814469373df595051a43a4bfcc5
mustafasaied97 <mustafasaied97@gmail.com>
2023-03-28 03:02:09 +0200

    add new logo

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const root=ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 )
